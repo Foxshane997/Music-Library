ac3d8d7ae1e4b33b46c9f9d6f1909a3b7b5fe163
“Foxshane997” <Foxshane997@gmail.com>
2024-06-15 10:20:45 -0700

    Added artist & album view but they are fetch erroring

--- src/Components/ArtistView.js
+++ src/Components/ArtistView.js
@@ -1,7 +1,49 @@
-// These components will be making separate API calls from the app.
-// component to serve specific data about our artist.
+// // These components will be making separate API calls from the app.
+// // component to serve specific data about our artist.
+// import { useState, useEffect } from "react";
+// import { useParams } from "react-router-dom";
+
+// function ArtistView() {
+//   const { id } = useParams();
+//   const [artistData, setArtistData] = useState([]);
+
+//   useEffect(() => {
+//     const API_URL = `http://localhost:4000/album/${id}`;
+//     const fetchData = async () => {
+//       const response = await fetch(API_URL);
+//       const resData = await response.json();
+//       setArtistData(resData.results);
+//     };
+//     fetchData();
+//   }, [id]);
+
+//   const justAlbums = artistData.filter(
+//     (entry) => entry.collectionType === "Album"
+//   );
+
+//   const renderAlbums = justAlbums.map((album, i) => {
+//     return (
+//       <div>
+//         <link to={`/album/${album.collectionId}`}>
+//           <p>{album.collectionName}</p>
+//         </link>
+//       </div>
+//     );
+//   });
+
+//   return (
+//     <div>
+//       <h2>The id passed was: {id}</h2>
+//       <p>Artist Data Goes Here!</p>
+//       {renderAlbums}
+//     </div>
+//   );
+// }
+
+// export default ArtistView;
+
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 function ArtistView() {
   const { id } = useParams();
@@ -23,10 +65,10 @@ function ArtistView() {
 
   const renderAlbums = justAlbums.map((album, i) => {
     return (
-      <div>
-        <link to={`/album/${album.collectionId}`}>
+      <div key={album.collectionId}>
+        <Link to={`/album/${album.collectionId}`}>
           <p>{album.collectionName}</p>
-        </link>
+        </Link>
       </div>
     );
   });
