9262bb8cd0754b9212835247c87cef95bfc43a0f
“Foxshane997” <Foxshane997@gmail.com>
2024-06-17 17:53:53 -0700

    Added the fix thanks John!

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,15 +1,16 @@
+// 
+
 import React, { useContext } from "react";
 import { DataContext } from "../Context/DataContext";
 import GalleryItem from "./GalleryItem";
 
 function Gallery(props) {
-  const data = useContext(DataContext);
 
-  const display = data.map((item, index) => {
-    return <GalleryItem item={item} key={index} />;
-  });
+const display = props.data.map((item, index) => {
+return <GalleryItem item={item} key={index} />;
+});
 
-  return <div>{display}</div>;
+return <div>{display}</div>;
 }
 
 export default Gallery;
